f9ae295a4abd2e233df99ae63220e8ec66a95be9
otis <leminhhieu98py@gmail.com>
2022-07-18 14:31:47 +0700

    setup esbuild and simple transpile

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 function App() {
+  const [userCode, setUserCode] = useState<string>('');
+  const [compileCode, setCompileCode] = useState<string>('');
+  const esbuildRef = useRef<any>(null);
+
+  useEffect(() => {
+    startEsbuild();
+  }, []);
+
+  const startEsbuild = async () => {
+    esbuildRef.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  const handleClick = async () => {
+    if (!esbuildRef.current) {
+      return;
+    }
+
+    const result = await esbuildRef.current.transform(userCode, {
+      loader: 'jsx',
+      target: 'es2015'
+    });
+
+    setCompileCode(result?.code);
+  };
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <textarea
+        onChange={(e) => setUserCode(e.target.value)}
+        value={userCode}
+      />
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre>{compileCode}</pre>
     </div>
   );
 }
 
-export default App;
+export default React.memo(App, () => true);
