40305587b82aa4f010b7e2ab53c32006a0d99857
leminhhieu98py <leminhhieu98py@gmail.com>
2022-07-19 22:47:02 +0700

    Handle plugin with nested import

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -8,27 +8,42 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       // build.onResolve is to find out where is the file in our local system
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
+        console.log('onResolve', args);
+
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
 
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, `${args.importer}/`).href
+            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href
           };
         }
 
-        return { path: `${UNPACKAGE_URL}react`, namespace: 'a' };
-        // return { path: `${UNPACKAGE_URL}react/${args.path}`, namespace: 'a' };
+        return { path: `${UNPACKAGE_URL}${args.path}`, namespace: 'a' };
       });
 
       // build.onLoad is trigger whenever a resolve is triggered -  I mean that the file is loaded
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
-        const { data } = await axios.get(args.path);
+
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+              const message = require('react');
+              console.log(message);
+            `
+          };
+        }
+
+        const { data, request } = await axios.get(args.path);
 
         return {
           loader: 'jsx',
-          contents: data
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         };
       });
     }
