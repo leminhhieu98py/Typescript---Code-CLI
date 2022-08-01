7677e0d51bed7ea6a79439bf3e77b08d247a8b5d
otis <leminhhieu98py@gmail.com>
2022-08-01 16:56:41 +0700

    Handle css import + check cache for both

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -2,6 +2,12 @@ import * as esbuild from 'esbuild-wasm';
 import localforage from 'localforage';
 import axios from 'axios';
 
+const useCacheData = async (path: string) => {
+  const cacheData = await localforage.getItem<esbuild.OnLoadResult>(path);
+  const isCache = !!cacheData;
+  return [isCache, cacheData];
+};
+
 export const fetchPlugin = (userCode: string) => {
   return {
     name: 'fetch-plugin',
@@ -15,9 +21,8 @@ export const fetchPlugin = (userCode: string) => {
         };
       });
 
-      // Handle import module
+      // Check whether data is cached
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        // Check whether data is cached
         const cacheData = await localforage.getItem<esbuild.OnLoadResult>(
           args.path
         );
@@ -25,7 +30,36 @@ export const fetchPlugin = (userCode: string) => {
         if (cacheData) {
           return cacheData;
         }
+      });
 
+      // Handle css files only
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const escapeContent = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents = `
+            const styleTag = document.createElement('style');
+            styleTag.innerText = '${escapeContent}';
+            document.head.appendChild(styleTag)
+        `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname
+        };
+
+        await localforage.setItem(args.path, result);
+
+        return result;
+      });
+
+      // Handle import module
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
