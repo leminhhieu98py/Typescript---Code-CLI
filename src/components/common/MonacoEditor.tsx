09999db2898d9024e4b08a48b7b7e4f752da9095
leminhhieu98py <leminhhieu98py@gmail.com>
2022-08-21 22:41:42 +0700

    Implement prettier to code editor formatter

--- src/components/common/MonacoEditor.tsx
+++ src/components/common/MonacoEditor.tsx
@@ -1,36 +1,57 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Editor, { EditorDidMount } from '@monaco-editor/react';
-
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 interface MoncacoEditorProps {
   value: string;
   onChange: (value: string) => void;
 }
 
+const PRETTIER_FORMAT_OPTIONS = {
+  parser: 'babel',
+  plugins: [parser],
+  useTabs: false,
+  semi: true,
+  singleQuote: true
+};
+
 const MoncacoEditor: React.FC<MoncacoEditorProps> = ({ value, onChange }) => {
+  const editorRef = useRef<any>(null);
+
   const onEditorDidMount: EditorDidMount = (getEditorValue, editor) => {
+    editorRef.current = editor;
     editor.onDidChangeModelContent(() => {
       onChange(getEditorValue());
     });
   };
 
+  const handleFormatCode = () => {
+    const unformatCode = editorRef.current.getModel().getValue();
+    const formatCode = prettier.format(unformatCode, PRETTIER_FORMAT_OPTIONS);
+    onChange(formatCode)
+  };
+
   return (
-    <Editor
-      editorDidMount={onEditorDidMount}
-      value={value}
-      theme="dark"
-      height={500}
-      language="javascript"
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true
-      }}
-    />
+    <>
+      <button onClick={handleFormatCode}>Format</button>
+      <Editor
+        editorDidMount={onEditorDidMount}
+        value={value}
+        theme="dark"
+        height={500}
+        language="javascript"
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true
+        }}
+      />
+    </>
   );
 };
 
