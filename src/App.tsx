03c94f6aaa85db2410f2fee378e76131eaba63d1
leminhhieu98py <leminhhieu98py@gmail.com>
2022-08-22 22:35:07 +0700

    refactor code for compile code screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import { ESBUILD_WASM_URL } from './common/const';
 import MoncacoEditor from './components/common/MonacoEditor/MonacoEditor';
 import CompileCodeScreen from './components/CompileCodeScreen';
@@ -14,32 +12,10 @@ const initialEditorValue = `import React from 'react';
   root.render(<h1>Hello World</h1>)
 `;
 
-const iframeSrcDoc = `
-  <head></head>
-  <body>
-    <div id="root"></div>
-    <script>
-      window.addEventListener('message', (event) => {
-        try {
-          eval(event.data);
-        } catch (err) {
-          const root = document.getElementById('root');
-          root.innerHTML = '<div style="color: red;"><h4>Runtime error:</h4>' + err + '</div>';
-          console.error(err);
-        }
-      }, false)
-    </script>
-  </body>
-`;
-
 function App() {
   const [userCode, setUserCode] = useState<string>(initialEditorValue);
   const esbuildRef = useRef<any>(null);
-  const iframeRef = useRef<any>(null);
 
-  useEffect(() => {
-    startEsbuild();
-  }, []);
   const startEsbuild = async () => {
     esbuildRef.current = await esbuild.startService({
       worker: true,
@@ -47,47 +23,14 @@ function App() {
     });
   };
 
-  const resetIframeContent = useCallback(() => {
-    iframeRef.current.srcdoc = iframeSrcDoc;
-  }, []);
-
-  const handleClick = useCallback(
-    async (userCode: string) => {
-      if (!esbuildRef.current) {
-        return;
-      }
-
-      resetIframeContent();
-
-      const result = await esbuildRef.current.build({
-        entryPoints: ['index.js'],
-        bundle: true,
-        write: false,
-        plugins: [unpkgPathPlugin(), fetchPlugin(userCode)],
-        define: {
-          'process.env.NODE_ENV': '"production"',
-          global: 'window'
-        }
-      });
-
-      iframeRef.current.contentWindow.postMessage(
-        result.outputFiles[0].text,
-        '*'
-      );
-    },
-    [resetIframeContent]
-  );
-
   useEffect(() => {
-    const bundleCode = setTimeout(() => handleClick(userCode), 1000);
-
-    return () => clearTimeout(bundleCode);
-  }, [userCode, handleClick]);
+    startEsbuild();
+  }, []);
 
   return (
     <div>
       <MoncacoEditor value={userCode} onChange={setUserCode} />
-      <CompileCodeScreen ref={iframeRef} srcDoc={iframeSrcDoc} />
+      <CompileCodeScreen userCode={userCode} esbuildRef={esbuildRef} />
     </div>
   );
 }
--- src/components/CompileCodeScreen.tsx
+++ src/components/CompileCodeScreen.tsx
@@ -1,20 +1,80 @@
-import React, { forwardRef } from 'react';
-
+import React, { useEffect, useCallback, useRef, forwardRef } from 'react';
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
+import { fetchPlugin } from '../plugins/fetch-plugin';
 interface CompileCodeScreenProps {
-  srcDoc: string;
+  userCode: string;
+  esbuildRef: any;
 }
 
-const CompileCodeScreen = forwardRef<HTMLIFrameElement, CompileCodeScreenProps>(
-  ({ srcDoc }, ref: React.Ref<HTMLIFrameElement>) => {
-    return (
-      <iframe
-        sandbox="allow-scripts"
-        srcDoc={srcDoc}
-        ref={ref}
-        title="code preview"
-      />
-    );
-  }
-);
+const iframeSrcDoc = `
+  <head></head>
+  <body>
+    <div id="root"></div>
+    <script>
+      window.addEventListener('message', (event) => {
+        try {
+          eval(event.data);
+        } catch (err) {
+          const root = document.getElementById('root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime error:</h4>' + err + '</div>';
+          console.error(err);
+        }
+      }, false)
+    </script>
+  </body>
+`;
+
+const CompileCodeScreen: React.FC<CompileCodeScreenProps> = ({
+  userCode,
+  esbuildRef
+}) => {
+  const iframeRef = useRef<any>(null);
+
+  const resetIframeContent = useCallback(() => {
+    iframeRef.current.srcdoc = iframeSrcDoc;
+  }, []);
+
+  const handleCodeChange = useCallback(
+    async (userCode: string) => {
+      if (!esbuildRef.current) {
+        return;
+      }
+
+      resetIframeContent();
+
+      const result = await esbuildRef.current.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [unpkgPathPlugin(), fetchPlugin(userCode)],
+        define: {
+          'process.env.NODE_ENV': '"production"',
+          global: 'window'
+        }
+      });
+
+      iframeRef.current.contentWindow.postMessage(
+        result.outputFiles[0].text,
+        '*'
+      );
+    },
+    [esbuildRef, resetIframeContent]
+  );
+
+  useEffect(() => {
+    const bundleCode = setTimeout(() => handleCodeChange(userCode), 1000);
+
+    return () => clearTimeout(bundleCode);
+  }, [userCode, handleCodeChange]);
+
+  return (
+    <iframe
+      ref={iframeRef}
+      sandbox="allow-scripts"
+      srcDoc={iframeSrcDoc}
+      title="code preview"
+    />
+  );
+};
 
 export default CompileCodeScreen;
