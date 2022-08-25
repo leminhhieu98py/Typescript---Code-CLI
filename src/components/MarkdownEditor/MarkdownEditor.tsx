b7501aba48807b7bd494663741edd0b875fd22f7
Lê Minh Hiếu (Otis) <69639199+leminhhieu98py@users.noreply.github.com>
2022-08-25 17:15:19 +0700

    Feature/markdown editor (#1)
    * install react-md-editor
    
    * add MD editor in editmode and viewmode
    
    * Implement MD editor into App
    
    * overwrite default css of bulma
    
    * fix divider height in editor
    
    * custome markdown editor resize bar
    
    * additional markdown styling
    
    * implement dark theme
    
    * add state to markdown editor
    
    * update styling
    
    * clean code

---
+++ src/components/MarkdownEditor/MarkdownEditor.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import './markdownEditor.css';
+
+const MarkdownEditor = () => {
+  const [content, setContent] = useState<string>('');
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const mdEditorRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleToggleEditMode = (e: MouseEvent) => {
+      if (
+        mdEditorRef.current &&
+        e.target &&
+        mdEditorRef.current.contains(e.target as Node)
+      ) {
+        setIsEditing(true);
+        return;
+      }
+
+      setIsEditing(false);
+    };
+    window.addEventListener('click', handleToggleEditMode, { capture: true });
+
+    return () =>
+      window.removeEventListener('click', handleToggleEditMode, {
+        capture: true
+      });
+  }, []);
+
+  return (
+    <div className="text-editor card" ref={mdEditorRef}>
+      <div className="card-content">
+        {isEditing ? (
+          <MDEditor value={content} onChange={(e) => setContent(e as string)} />
+        ) : (
+          <MDEditor.Markdown
+            source={content ? content : `### Start to write a document here`}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MarkdownEditor;
