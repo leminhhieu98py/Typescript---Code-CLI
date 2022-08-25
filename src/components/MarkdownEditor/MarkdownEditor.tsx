583d9e5192840ed4258e7c7fd94c737494bf1935
otis <leminhhieu98py@gmail.com>
2022-08-25 17:02:39 +0700

    add state to markdown editor

--- src/components/MarkdownEditor/MarkdownEditor.tsx
+++ src/components/MarkdownEditor/MarkdownEditor.tsx
@@ -3,6 +3,7 @@ import MDEditor from '@uiw/react-md-editor';
 import './markdownEditor.css';
 
 const MarkdownEditor = () => {
+  const [content, setContent] = useState<string>('');
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const mdEditorRef = useRef<HTMLDivElement | null>(null);
 
@@ -29,7 +30,11 @@ const MarkdownEditor = () => {
 
   return (
     <div className="text-editor" ref={mdEditorRef}>
-      {isEditing ? <MDEditor /> : <MDEditor.Markdown source="# hello" />}
+      {isEditing ? (
+        <MDEditor value={content} onChange={(e) => setContent(e as string)} />
+      ) : (
+        <MDEditor.Markdown source={content ? content : `### Start to write a document here`} />
+      )}
     </div>
   );
 };
