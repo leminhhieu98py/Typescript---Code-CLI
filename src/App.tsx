cc46f51d2dbdbfd54969b5c3e485502847aaa39c
otis <leminhhieu98py@gmail.com>
2022-08-29 10:16:13 +0700

    add Provider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import CodeCell from './components/CodeCell/CodeCell';
 import MarkdownEditor from './components/MarkdownEditor/MarkdownEditor';
+import store from './store/store';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <MarkdownEditor />
       <CodeCell />
-    </>
+    </Provider>
   );
 };
 
