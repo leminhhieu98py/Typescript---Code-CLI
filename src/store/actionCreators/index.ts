0d7cffb5be4d18d13c61dab6227d11ae4569b8dc
otis <leminhhieu98py@gmail.com>
2022-08-29 10:47:14 +0700

    add some simple creators

---
+++ src/store/actionCreators/index.ts
@@ -0,0 +1,51 @@
+import {
+  MoveCellAction,
+  UpdateCellAction,
+  DeleteCellAction,
+  InsertCellBeforeAction,
+  Direction
+} from '../actions';
+import { ActionType } from '../actionTypes';
+import { CellType } from '../interfaces/Cell';
+
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction
+    }
+  };
+};
+
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content
+    }
+  };
+};
+
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: {
+      id
+    }
+  };
+};
+
+export const insertCellBefore = (
+  id: string,
+  type: CellType
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type
+    }
+  };
+};
