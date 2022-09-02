9fb4e6283fe64193b3de2925ce72c42016aebcb6
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-02 23:18:34 +0700

    add action type

--- src/store/actionTypes/index.ts
+++ src/store/actionTypes/index.ts
@@ -2,5 +2,7 @@ export enum ActionType {
   MOVE_CELL = 'move_cell',
   UPDATE_CELL = 'update_cell',
   DELETE_CELL = 'delete_cell',
-  INSERT_CELL_AFTER = 'insert_cell_after'
+  INSERT_CELL_AFTER = 'insert_cell_after',
+  START_BUNDLING = 'start_bundling',
+  STOP_BUNDLING = 'stop_bundling'
 }
