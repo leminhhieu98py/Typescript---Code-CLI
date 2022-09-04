60dd147e083cb846037688d95c7ab2c749f451e9
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-04 23:26:45 +0700

    remove unnecessary code

--- src/components/CodeCell/CodeCell.tsx
+++ src/components/CodeCell/CodeCell.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './codeCell.css';
 import MoncacoEditor from '../common/MonacoEditor/MonacoEditor';
 import ResizableContainer from '../common/ResizableContainer/ResizableContainer';
