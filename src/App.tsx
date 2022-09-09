17055494e00f7949e99582f30b0bd7a17734d3cb
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-09 19:57:48 +0700

    test import antd css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
+// import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import './App.scss';
 import React from 'react';
 import { Provider } from 'react-redux';
 import CellList from './components/CellList/CellList';
+import { Button } from 'antd';
 
 import store from './store/store';
 
@@ -10,6 +12,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <CellList />
+      <Button type="primary">Primary Button</Button>
     </Provider>
   );
 };
