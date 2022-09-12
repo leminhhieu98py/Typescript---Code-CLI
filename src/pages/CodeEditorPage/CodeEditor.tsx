064fffcd23df970413e68339bb0669b35af58b19
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-13 00:23:00 +0700

    move codecell component into CodeEditor page

---
+++ src/pages/CodeEditorPage/CodeEditor.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './CodeEditor.css';
+import MoncacoEditor from '../../components/common/MonacoEditor/MonacoEditor';
+import ResizableContainer from '../../components/common/ResizableContainer/ResizableContainer';
+import CompileCodeScreen from '../../components/CompileCodeScreen/CompileCodeScreen';
+
+const initialEditorValue = `import { useState } from 'react';
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+  return (
+    <>
+      <span>Count: {count}</span>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+    </>
+  );
+};
+
+show(<App />)`;
+
+const CodeEditor = () => {
+  const [codeInput, setCodeInput] = useState<string>(initialEditorValue);
+
+  return (
+    <div>
+      <div className="code-shell-container">
+        <ResizableContainer direction="horizontal">
+          <MoncacoEditor value={codeInput} onChange={setCodeInput} />
+        </ResizableContainer>
+        {/* TODO: handle id later */}
+        <CompileCodeScreen id={'1'} userCode={codeInput} />
+      </div>
+    </div>
+  );
+};
+
+export default CodeEditor;
