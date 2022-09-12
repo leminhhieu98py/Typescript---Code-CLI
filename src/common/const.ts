e12170f5e947f16851e0f0cac17278ce305cbe04
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-12 23:55:50 +0700

    remove common folder, declare common constants in utils folder

--- src/common/const.ts
+++
@@ -1,3 +0,0 @@
-const ESBUILD_WASM_URL = 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm';
-
-export { ESBUILD_WASM_URL };
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,3 +1,4 @@
 const UNPACKAGE_URL = 'https://unpkg.com/';
+const ESBUILD_WASM_URL = 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm';
 
-export { UNPACKAGE_URL };
+export { UNPACKAGE_URL, ESBUILD_WASM_URL };
