3e4627f5f46b027737e5777a4d14ca62a4228968
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-13 00:32:29 +0700

    remove routes hard-code

--- src/layouts/HomepageLayout/HomepageHeader.tsx
+++ src/layouts/HomepageLayout/HomepageHeader.tsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import { Layout } from 'antd';
+import ROUTES_PATH from './../../router/routesPath';
 const { Header } = Layout;
 
 const HomepageHeader = () => {
   return (
     <Header>
       <Link to="/">Home</Link>
-      <Link to="/cells">Start coding</Link>
+      <Link to={ROUTES_PATH.CODE_EDITOR}>Start coding</Link>
     </Header>
   );
 };
