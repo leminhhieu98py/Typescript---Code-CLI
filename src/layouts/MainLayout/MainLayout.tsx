772096717b740f58b0a74c2edfab0b29e68475e1
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-13 23:26:20 +0700

    update code editor UI component

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -2,16 +2,17 @@ import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
 import MainLayoutSider from './MainLayoutSider';
 import MainLayoutHeader from './MainLayoutHeader';
+import styles from './MainLayout.module.scss';
 
 const { Content } = Layout;
 
 const MainLayout = () => {
   return (
-    <Layout>
+    <Layout className={styles['layout__wrapper']}>
       <MainLayoutSider />
       <Layout>
         <MainLayoutHeader />
-        <Content>
+        <Content className={styles['layout__content__wrapper']}>
           <Outlet />
         </Content>
       </Layout>
