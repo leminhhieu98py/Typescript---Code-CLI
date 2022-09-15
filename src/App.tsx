b377b64daba96412d77cc350d12cc5a4f072a77c
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-16 01:01:41 +0700

    using less instead of scss to custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import './App.scss';
+import './less/app.less';
 
 import store from './store/store';
 import routes from './router/routes';
